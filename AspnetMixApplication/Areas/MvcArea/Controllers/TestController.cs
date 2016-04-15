99a28e1703197fafe1dc0291d3caa94cfd9edabc
Sergey Koshelev <xih@yandex.ru>
2016-04-15 15:07:47 +0300

    Asp.Net MVC and WebForms mix using Areas

---
+++ AspnetMixApplication/Areas/MvcArea/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspnetMixApplication.Areas.MvcArea.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: MvcArea/Test
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "This is ASP.NET MVC!";
+            return View();
+        }
+    }
+}
---
+++ AspnetMixApplication/Areas/MvcArea/MvcAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AspnetMixApplication.Areas.MvcArea
+{
+    public class MvcAreaAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "MvcArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "MvcArea_default",
+                "MvcArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
