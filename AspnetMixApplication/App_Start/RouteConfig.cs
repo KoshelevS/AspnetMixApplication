126f66b50cf68dc8cdef69b1f32dc7cb18e9671f
Sergey Koshelev <xih@yandex.ru>
2016-04-15 14:46:08 +0300

    ASP.NET MVC and WebForms in-place mix

--- AspnetMixApplication/App_Start/RouteConfig.cs
+++ AspnetMixApplication/App_Start/RouteConfig.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 using Microsoft.AspNet.FriendlyUrls;
 
@@ -10,9 +11,23 @@ namespace AspnetMixApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            // WebForms routes
             var settings = new FriendlyUrlSettings();
             settings.AutoRedirectMode = RedirectMode.Permanent;
             routes.EnableFriendlyUrls(settings);
+
+            // MVC routes
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+//            routes.IgnoreRoute("{resource}.aspx/{*pathInfo}");
+
+            routes.MapRoute(
+               "Default",
+               // Route name
+               "mvc/{controller}/{action}/{id}",
+               // URL with parameters
+               new { controller = "Home", action = "Index", id = "" }
+                // Parameter defaults
+                );
         }
     }
 }
