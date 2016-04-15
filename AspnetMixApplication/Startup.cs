d6f0675b7b368b65a03819294fc001fe71028980
Sergey Koshelev <xih@yandex.ru>
2016-04-15 13:36:54 +0300

    Create default WebForms application

---
+++ AspnetMixApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspnetMixApplication.Startup))]
+namespace AspnetMixApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
